3747327755bd9397e0324480bd9c92f17264f39e
patrick-putz <Putz.Patrick.1996@gmail.com>
2023-01-21 10:36:25 +0100

    Implemented getLight
    closes #1

--- ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
+++ ledController/src/main/java/at/edu/c02/ledcontroller/LedControllerImpl.java
@@ -29,4 +29,23 @@ public class LedControllerImpl implements LedController {
         System.out.println("First light id is: " + firstLight.getInt("id"));
         System.out.println("First light color is: " + firstLight.getString("color"));
     }
+
+    @Override
+    public void demoId(int id) throws IOException {
+        // Call `getLights`, the response is a json object in the form `{ "lights": [ { ... }, { ... } ] }`
+        JSONObject response = apiService.getLight(id);
+        // get the "lights" array from the response
+        JSONArray lights = response.getJSONArray("lights");
+        if (lights.length() < 1){
+            System.out.println("Specified id does not exist!");
+        }
+        else{
+            // read the first json object of the lights array
+            JSONObject lightByID = lights.getJSONObject(0);
+            // read int and string properties of the light
+            System.out.println("light id is: " + lightByID.getInt("id"));
+            System.out.println("light color is: " + lightByID.getString("color"));
+        }
+    }
+
 }
